ef8b7544145bfa99e831856f60b483de4fec2646
陈琪 <chenqi@jimistore.com>
2018-03-08 16:37:17 +0800

    新增nemo-mq-core-api模块

---
+++ nemo-mq-core-api/src/test/java/com/jimistore/boot/nemomqcoreapi/NemoMqCoreApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jimistore.boot.nemomqcoreapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NemoMqCoreApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
