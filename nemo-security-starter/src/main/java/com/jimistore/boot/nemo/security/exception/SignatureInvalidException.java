9668b32601b9650d8bf78f84055ca784887d6592
陈琪 <chenqi@jimistore.com>
2018-04-27 11:31:40 +0800

    修改token验证异常的错误代码

--- nemo-security-starter/src/main/java/com/jimistore/boot/nemo/security/exception/SignatureInvalidException.java
+++ nemo-security-starter/src/main/java/com/jimistore/boot/nemo/security/exception/SignatureInvalidException.java
@@ -6,19 +6,24 @@ public class SignatureInvalidException extends UnauthorizedException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private static final String ERROR_CODE="40102";
 		
 	private static final String ERROR_MSG="接口调用鉴权签名校验异常";
 
 	public SignatureInvalidException(Throwable cause) {
 		super(cause);
+		super.setCode(ERROR_CODE);
 	}
 
 	public SignatureInvalidException(String message) {
 		super(String.format("%s==>%s", ERROR_MSG, message));
+		super.setCode(ERROR_CODE);
 	}
 
 	public SignatureInvalidException() {
 		super(ERROR_MSG);
+		super.setCode(ERROR_CODE);
 	}
 	
 	
--- nemo-security-starter/src/main/java/com/jimistore/boot/nemo/security/exception/TokenInvalidException.java
+++ nemo-security-starter/src/main/java/com/jimistore/boot/nemo/security/exception/TokenInvalidException.java
@@ -6,19 +6,24 @@ public class TokenInvalidException extends UnauthorizedException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private static final String ERROR_CODE="40101";
 		
 	private static final String ERROR_MSG="接口调用鉴权令牌校验异常";
 
 	public TokenInvalidException(Throwable cause) {
 		super(cause);
+		super.setCode(ERROR_CODE);
 	}
 
 	public TokenInvalidException(String message) {
 		super(String.format("%s==>%s", ERROR_MSG, message));
+		super.setCode(ERROR_CODE);
 	}
 
 	public TokenInvalidException() {
 		super(ERROR_MSG);
+		super.setCode(ERROR_CODE);
 	}
 	
 	
