401533a5328dd81f55568cfbe04142965c3d7241
chenqi <chenqi@192.168.11.20>
2019-02-25 10:59:39 +0800

    优化eureka集群配置

--- nemo-monitor-server/src/main/java/com/jimistore/boot/nemo/monitor/server/config/SecurityConfig.java
+++ nemo-monitor-server/src/main/java/com/jimistore/boot/nemo/monitor/server/config/SecurityConfig.java
@@ -1,8 +1,17 @@
 package com.jimistore.boot.nemo.monitor.server.config;
 
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
+
+import com.jimistore.boot.nemo.monitor.server.helper.TextXmlMappingJackson2HttpMessageConverter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,5 +28,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// Enable so that the clients can authenticate via HTTP basic for
 		// registering
 		http.httpBasic();
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(RestTemplate.class)
+	public RestTemplate restTemplate() {
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		requestFactory.setReadTimeout(10000);
+		requestFactory.setConnectTimeout(3000);
+
+		// 支持text/xml方式的json response
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
+		restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+		restTemplate.getMessageConverters().add(new TextXmlMappingJackson2HttpMessageConverter());
+		return restTemplate;
 	}
 }
