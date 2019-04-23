420f1f5e2d022ffb421a8264db61b154f4f022f1
chenqi <chenqi@192.168.11.131>
2019-04-23 18:05:44 +0800

    修复context因线程池原因被复用的bug

--- nemo-core/src/main/java/com/jimistore/boot/nemo/core/helper/InitContextFilter.java
+++ nemo-core/src/main/java/com/jimistore/boot/nemo/core/helper/InitContextFilter.java
@@ -57,7 +57,7 @@ public class InitContextFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-
+		Context.init();
 		for (Context.Key key : Context.Key.values()) {
 			try {
 				String code = key.getCode();
