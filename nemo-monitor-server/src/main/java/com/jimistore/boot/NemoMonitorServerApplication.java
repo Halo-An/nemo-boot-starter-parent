e0b140f555fbcc5592e437d4300bcace68daa74a
chenqi <chenqi@192.168.11.131>
2019-05-14 16:57:12 +0800

    eureka-server和monitor-server支持tomcat部署

--- nemo-monitor-server/src/main/java/com/jimistore/boot/NemoMonitorServerApplication.java
+++ nemo-monitor-server/src/main/java/com/jimistore/boot/NemoMonitorServerApplication.java
@@ -2,6 +2,8 @@ package com.jimistore.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
@@ -9,7 +11,13 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 @EnableAdminServer
 @EnableEurekaClient
 @SpringBootApplication
-public class NemoMonitorServerApplication {
+public class NemoMonitorServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(NemoMonitorServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NemoMonitorServerApplication.class, args);
--- nemo-rpc-eureka-server/src/main/java/com/jimistore/boot/NemoRpcEurekaServerApplication.java
+++ nemo-rpc-eureka-server/src/main/java/com/jimistore/boot/NemoRpcEurekaServerApplication.java
@@ -5,6 +5,8 @@ import java.nio.charset.StandardCharsets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,21 +17,27 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaServer
 @Configuration
-public class NemoRpcEurekaServerApplication {
+public class NemoRpcEurekaServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NemoRpcEurekaServerApplication.class, args);
 	}
-	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(NemoRpcEurekaServerApplication.class);
+	}
+
 	@Bean
 	@ConditionalOnMissingBean(RestTemplate.class)
-	public RestTemplate restTemplate(){
+	public RestTemplate restTemplate() {
 		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setReadTimeout(10000);
-        requestFactory.setConnectTimeout(3000);
+		requestFactory.setReadTimeout(10000);
+		requestFactory.setConnectTimeout(3000);
 
-        //支持text/xml方式的json response
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
+		// 支持text/xml方式的json response
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
 		restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
 		return restTemplate;
 	}
