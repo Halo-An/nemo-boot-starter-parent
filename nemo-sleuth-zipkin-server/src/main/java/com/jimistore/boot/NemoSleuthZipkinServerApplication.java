8a546565decf4b6d4a10d1e6d210eecc649f1fee
hexin <hexin@jimistore.com>
2019-12-02 15:24:18 +0800

    更新zipkin

--- nemo-sleuth-zipkin-server/src/main/java/com/jimistore/boot/NemoSleuthZipkinServerApplication.java
+++ nemo-sleuth-zipkin-server/src/main/java/com/jimistore/boot/NemoSleuthZipkinServerApplication.java
@@ -2,13 +2,18 @@ package com.jimistore.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import zipkin.server.EnableZipkinServer;
 
+@EnableZipkinServer
 @SpringBootApplication
-@EnableEurekaClient
-@EnableZipkinStreamServer
-public class NemoSleuthZipkinServerApplication {
+public class NemoSleuthZipkinServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(NemoSleuthZipkinServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NemoSleuthZipkinServerApplication.class, args);
