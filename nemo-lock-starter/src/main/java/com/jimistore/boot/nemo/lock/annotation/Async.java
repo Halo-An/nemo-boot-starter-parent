0f2ecc808cb159e33cf2aa8f570631aba54b07c3
陈琪 <332846454@qq.com>
2020-01-06 16:16:58 +0800

    修改异步任务的注释

--- nemo-lock-starter/src/main/java/com/jimistore/boot/nemo/lock/annotation/Async.java
+++ nemo-lock-starter/src/main/java/com/jimistore/boot/nemo/lock/annotation/Async.java
@@ -6,27 +6,30 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD})
+@Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Async {
-	
+
 	String value() default "";
-	
+
 	/**
-	 * 初始最大线程容量
+	 * 初始线程容量
+	 * 
 	 * @return
 	 */
 	int capacity() default 10;
-	
+
 	/**
-	 * 超出队列最大限制后的最大线程容量
+	 * 最大线程容量
+	 * 
 	 * @return
 	 */
 	int maxCapacity() default 10;
-	
+
 	/**
 	 * 初始最大线程容量满后存队列的容量
+	 * 
 	 * @return
 	 */
 	int queueCapacity() default Integer.MAX_VALUE;
