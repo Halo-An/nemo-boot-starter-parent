f929830c2013e988b976b58beb0176283742803f
陈琪 <332846454@qq.com>
2020-06-02 14:24:15 +0800

    现在所有异常都会输出成格式{“code”:"“,”message“:”异常"}

--- nemo-core/src/main/java/com/jimistore/boot/nemo/core/helper/ResponseExceptionHandle.java
+++ nemo-core/src/main/java/com/jimistore/boot/nemo/core/helper/ResponseExceptionHandle.java
@@ -2,6 +2,7 @@ package com.jimistore.boot.nemo.core.helper;
 
 import java.io.IOException;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,37 +10,60 @@ import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
 import org.apache.log4j.Logger;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.jimistore.boot.nemo.core.api.exception.ValidatedException;
 import com.jimistore.boot.nemo.core.response.Response;
 import com.jimistore.boot.nemo.core.util.JsonString;
 
-public class ResponseExceptionHandle implements HandlerExceptionResolver  {
-	
+public class ResponseExceptionHandle implements HandlerExceptionResolver, PriorityOrdered {
+
 	private static Logger log = Logger.getLogger(ResponseExceptionHandle.class);
 
 	@Override
-	public ModelAndView resolveException(HttpServletRequest request,
-			HttpServletResponse response, Object handler, Exception ex) {
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception ex) {
 		log.error(ex.getMessage(), ex);
 		response.setCharacterEncoding("utf-8");
 		response.setContentType("application/json");
-		
-		if(ex instanceof UndeclaredThrowableException){
-			ex = (Exception) ((UndeclaredThrowableException)ex).getUndeclaredThrowable();
+
+		if (ex instanceof UndeclaredThrowableException) {
+			ex = (Exception) ((UndeclaredThrowableException) ex).getUndeclaredThrowable();
 		}
-		
-		if(ex instanceof ConstraintViolationException){
+
+		if (ex instanceof ConstraintViolationException) {
 			ConstraintViolationException mcve = (ConstraintViolationException) ex;
 			@SuppressWarnings("rawtypes")
 			ConstraintViolation cv = mcve.getConstraintViolations().iterator().next();
 			String[] fields = cv.getPropertyPath().toString().split("\\.");
-			String field = fields[fields.length-1];
-			ex = new ValidatedException(new StringBuilder(cv.getMessage()).append(field).toString(),ex);
+			String field = fields[fields.length - 1];
+			ex = new ValidatedException(new StringBuilder(cv.getMessage()).append(field).toString(), ex);
 		}
-		
+
+		if (ex instanceof HttpMessageNotReadableException) {
+			HttpMessageNotReadableException hmnre = (HttpMessageNotReadableException) ex;
+			if (hmnre.getCause() instanceof InvalidFormatException) {
+				InvalidFormatException ife = (InvalidFormatException) hmnre.getCause();
+				List<?> list = ife.getPath();
+				if (list != null && list.size() > 0) {
+					Object obj = list.get(0);
+					if (obj instanceof JsonMappingException.Reference) {
+						JsonMappingException.Reference ref = (JsonMappingException.Reference) obj;
+
+						ex = new ValidatedException(new StringBuilder(ref.getFieldName()).append(" must be ")
+								.append(ife.getTargetType().getSimpleName())
+								.toString(), ex);
+					}
+				}
+			}
+		}
+
 		try {
 			Response<?> resp = Response.error(ex);
 			response.getWriter().print(JsonString.toJson(resp));
@@ -47,6 +71,11 @@ public class ResponseExceptionHandle implements HandlerExceptionResolver  {
 			e.printStackTrace();
 		}
 		return new ModelAndView();
+	}
+
+	@Override
+	public int getOrder() {
+		return Ordered.HIGHEST_PRECEDENCE;
 	}
 
 }
