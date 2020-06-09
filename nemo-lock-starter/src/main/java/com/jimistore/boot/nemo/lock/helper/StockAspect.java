d3744735f22d33ced97001db31454f974b460edd
陈琪 <332846454@qq.com>
2020-06-09 14:31:23 +0800

    lock生产锁spel支持result

--- nemo-lock-starter/src/main/java/com/jimistore/boot/nemo/lock/helper/StockAspect.java
+++ nemo-lock-starter/src/main/java/com/jimistore/boot/nemo/lock/helper/StockAspect.java
@@ -1,6 +1,5 @@
 package com.jimistore.boot.nemo.lock.helper;
 
-
 import java.lang.reflect.Method;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -24,7 +23,7 @@ import com.jimistore.util.reflex.AnnotationUtil;
 @Aspect
 @Order(13)
 public class StockAspect {
-	
+
 	private StockHelper stockHelper;
 
 	public StockAspect setStockHelper(StockHelper stockHelper) {
@@ -33,100 +32,105 @@ public class StockAspect {
 	}
 
 	@Pointcut("@annotation(com.jimistore.boot.nemo.lock.annotation.LockConsume)")
-	public void consume(){
+	public void consume() {
 	}
 
 	@Pointcut("@annotation(com.jimistore.boot.nemo.lock.annotation.LockProduce)")
-	public void produce(){
+	public void produce() {
 	}
 
 	@Pointcut("@annotation(com.jimistore.boot.nemo.lock.annotation.LockStockUpdate)")
-	public void update(){
+	public void update() {
 	}
 
 	@Pointcut("@annotation(com.jimistore.boot.nemo.lock.annotation.LockStockCreate)")
-	public void create(){
+	public void create() {
 	}
-	
+
 	@Around("produce()")
-	public Object aroundProduce(ProceedingJoinPoint joinPoint) throws Throwable{
+	public Object aroundProduce(ProceedingJoinPoint joinPoint) throws Throwable {
+		Object obj = joinPoint.proceed();
+
 		Signature signature = joinPoint.getSignature();
 		MethodSignature methodSignature = (MethodSignature) signature;
 		Method method = methodSignature.getMethod();
-		
+
 		LockProduce lock = AnnotationUtil.getAnnotation(method, LockProduce.class);
-		StandardEvaluationContext context = this.getContextByProceedingJoinPoint(joinPoint);
+		StandardEvaluationContext context = this.getContextByProceedingJoinPoint(joinPoint, obj);
 		String key = this.parseExpression(context, lock.key(), String.class);
 		Long num = this.parseExpression(context, lock.num(), Long.class);
-		
-		Object obj = joinPoint.proceed();
+
 		stockHelper.produce(key, num);
 		return obj;
 	}
-	
+
 	@Around("create()")
-	public Object aroundCreate(ProceedingJoinPoint joinPoint) throws Throwable{
+	public Object aroundCreate(ProceedingJoinPoint joinPoint) throws Throwable {
+
+		Object obj = joinPoint.proceed();
+
 		Signature signature = joinPoint.getSignature();
 		MethodSignature methodSignature = (MethodSignature) signature;
 		Method method = methodSignature.getMethod();
-		
+
 		LockStockCreate lock = AnnotationUtil.getAnnotation(method, LockStockCreate.class);
-		StandardEvaluationContext context = this.getContextByProceedingJoinPoint(joinPoint);
+		StandardEvaluationContext context = this.getContextByProceedingJoinPoint(joinPoint, obj);
 		String key = this.parseExpression(context, lock.key(), String.class);
 		Long num = this.parseExpression(context, lock.num(), Long.class);
-		
-		Object obj = joinPoint.proceed();
 		stockHelper.create(key, num, lock.timeout());
 		return obj;
 	}
-	
+
 	@Around("update()")
-	public Object aroundUpdate(ProceedingJoinPoint joinPoint) throws Throwable{
+	public Object aroundUpdate(ProceedingJoinPoint joinPoint) throws Throwable {
+		Object obj = joinPoint.proceed();
+
 		Signature signature = joinPoint.getSignature();
 		MethodSignature methodSignature = (MethodSignature) signature;
 		Method method = methodSignature.getMethod();
-		
+
 		LockStockUpdate lock = AnnotationUtil.getAnnotation(method, LockStockUpdate.class);
-		StandardEvaluationContext context = this.getContextByProceedingJoinPoint(joinPoint);
+		StandardEvaluationContext context = this.getContextByProceedingJoinPoint(joinPoint, obj);
 		String key = this.parseExpression(context, lock.key(), String.class);
 		Long num = this.parseExpression(context, lock.num(), Long.class);
-		
-		Object obj = joinPoint.proceed();
+
 		stockHelper.cover(key, num, lock.timeout());
 		return obj;
 	}
-	
+
 	@Around("consume()")
-	public Object aroundConsume(ProceedingJoinPoint joinPoint) throws Throwable{
+	public Object aroundConsume(ProceedingJoinPoint joinPoint) throws Throwable {
 		Signature signature = joinPoint.getSignature();
 		MethodSignature methodSignature = (MethodSignature) signature;
 		Method method = methodSignature.getMethod();
-		
+
 		LockConsume lock = AnnotationUtil.getAnnotation(method, LockConsume.class);
-		StandardEvaluationContext context = this.getContextByProceedingJoinPoint(joinPoint);
+		StandardEvaluationContext context = this.getContextByProceedingJoinPoint(joinPoint, null);
 		String key = this.parseExpression(context, lock.key(), String.class);
 		Long num = this.parseExpression(context, lock.num(), Long.class);
 
 		stockHelper.consume(key, num, lock.prompt());
 		return joinPoint.proceed();
 	}
-	
+
 	/**
 	 * spel格式化
+	 * 
 	 * @param context
 	 * @param str
 	 * @return
 	 */
-	private <T> T parseExpression(StandardEvaluationContext context,String str, Class<T> clazz){
+	private <T> T parseExpression(StandardEvaluationContext context, String str, Class<T> clazz) {
 		return new SpelExpressionParser().parseExpression(str).getValue(context, clazz);
 	}
-	
+
 	/**
 	 * 获取函数spel对应初始化的上下文
+	 * 
 	 * @param joinPoint
 	 * @return
 	 */
-	private StandardEvaluationContext getContextByProceedingJoinPoint(ProceedingJoinPoint joinPoint){
+	private StandardEvaluationContext getContextByProceedingJoinPoint(ProceedingJoinPoint joinPoint, Object returnObj) {
 		Signature signature = joinPoint.getSignature();
 		MethodSignature methodSignature = (MethodSignature) signature;
 		Method method = methodSignature.getMethod();
@@ -134,14 +138,14 @@ public class StockAspect {
 		StandardEvaluationContext context = new StandardEvaluationContext();
 		ParameterNameDiscoverer parameterNameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
 		String[] parameterNames = parameterNameDiscoverer.getParameterNames(method);
-		for(int i=0;i<objs.length;i++){
+		context.setVariable("result", returnObj);
+		for (int i = 0; i < objs.length; i++) {
 			context.setVariable(String.format("%s%s", "p", i), objs[i]);
 		}
-		for(int i=0;i<objs.length;i++){
+		for (int i = 0; i < objs.length; i++) {
 			context.setVariable(parameterNames[i], objs[i]);
 		}
 		return context;
 	}
-	
-}
 
+}
